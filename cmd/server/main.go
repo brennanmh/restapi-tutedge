7ee1ecee2790a7413627c6e97ce10909ce242d90
Brennan Hildebrand <brennan.hildebrand@appdynamics.com>
2021-04-26 16:58:15 -0500

    Added App struct and Run() with App as receiver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,23 @@ package main
 
 import "fmt"
 
+// App - the strcut which contain things like pointers
+// to database connections.
+type App struct{}
+
+// Run - sets up our applications
+func (app *App) Run() error {
+	fmt.Println("Setting up our App")
+	return nil
+}
+
 func main() {
 
 	fmt.Println("Go REST API Course")
+
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error running rest service")
+		fmt.Println(err)
+	}
 }
